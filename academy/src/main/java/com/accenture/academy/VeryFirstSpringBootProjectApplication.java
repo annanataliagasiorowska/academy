d5899b7f889099d0d3de2174b1ff8a099184cb19
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2024-01-15 10:37:21 +0100

    Add initial project

---
+++ academy/src/main/java/com/accenture/academy/VeryFirstSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeryFirstSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeryFirstSpringBootProjectApplication.class, args);
+	}
+
+}
